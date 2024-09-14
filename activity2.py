f91ee0ce52ff3eda92ad70b9499195d81e8e1764
mustafa276 <mmustafamajeed1@gmail.com>
2024-09-14 11:39:42 +0500

    mathematical operations

---
+++ activity2.py
@@ -0,0 +1,7 @@
+import math
+print("the floor and ceiling value of 23.56 are" + str(math.ceil(23.56)) + "," + str(math.floor(23.56)))
+x = 10
+y = -15
+print("the value of x after it stole y's sign is " + str(math.copysign(x, y)))
+print("absolute value of -96 and 56 are " + str(math.fabs(-96)) + "," + str(math.fabs(56)))
+print("the HCF of 24 and 56 is " + str(math.gcd(24, 56)))
